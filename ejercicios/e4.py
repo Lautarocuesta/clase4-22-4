ff88a7bff2f5f28648f41165fd81f1ec67ddad25
Lautaro Cuesta <d47305409@instituto.itr.edu.ar>
2024-04-22 10:11:09 -0300

    correjidos todos

--- ejercicios/e4.py
+++ ejercicios/e4.py
@@ -1,3 +1,4 @@
+
 def palindromo(palabra):
   palabra = palabra.lower().replace(" ", "")
 
@@ -5,18 +6,16 @@ def palindromo(palabra):
   izquierda = 0
   derecha = len(palabra) - 1
 
-  
   while izquierda < derecha:
-    if palabra[izquierda] != palabra[derecha]:
+      if palabra[izquierda] != palabra[derecha]:
           return False
-  izquierda += 1
-  derecha -= 1
+      izquierda += 1
+      derecha -= 1
   return True
 
-
-#palabras
+# Palabras
 p1 = "oso"
-p2 = "plomo"
+p2 = "monos"
 
 print(f"¿'{p1}' es un palíndromo? {palindromo(p1)}")
 print(f"¿'{p2}' es un palíndromo? {palindromo(p2)}")
